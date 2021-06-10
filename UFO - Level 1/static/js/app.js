722f4490dad0576fc3d44a654c9e7bfc07cf5448
Carline Philippe <79476956+CPhilippe30@users.noreply.github.com>
2021-06-09 23:25:43 -0400

    Uploading

--- UFO - Level 1/static/js/app.js
+++ UFO - Level 1/static/js/app.js
@@ -10,3 +10,5 @@ var button = d3.select("#filter-btn");
 
 // Set values in input type date and time 
 var inputFieldDate = d3.select("#datetime");
+
+var columns = ["datetime", "city", "state", "country", "shape", "Duration", "comments"]
