0501df06f6c29738429b071c6ca248d3e9540cbd
Carline Philippe <79476956+CPhilippe30@users.noreply.github.com>
2021-06-10 00:41:26 -0400

    Uploading

--- UFO - Level 2/static/js/app.js
+++ UFO - Level 2/static/js/app.js
@@ -1,15 +1,22 @@
 // from data.js
 var tableData = data;
 
+// YOUR CODE HERE!
 
 var $tbody = d3.select("tbody");
+
+// Select the button
 var button = d3.select("#filter-btn");
+
+// Set values in input type date and time 
 var inputFieldDate = d3.select("#datetime");
+
+
 var inputFieldCity = d3.select("#city");
 
 var columns = ["datetime", "city", "state", "country", "shape", "duration", "comments"]
 
-
+// Input the data into the HTML
 var createData = (dataInput) => {
     dataInput.forEach(ufoSightings => {
         var row = $tbody.append("tr");
@@ -23,5 +30,4 @@ createData(tableData);
 button.on("click", () => {
     d3.event.preventDefault();
 
-    var inputDate = inputFieldDate.property("value").trim();
-    var inputCity = inputFieldCity.property("value").toLowerCase().trim();
+  
