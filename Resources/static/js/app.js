072be1f4dfea8cd456677359fa7c770396b17a51
Carline Philippe <79476956+CPhilippe30@users.noreply.github.com>
2021-06-09 23:19:57 -0400

    Uploading

--- Resources/static/js/app.js
+++ Resources/static/js/app.js
@@ -2,3 +2,12 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+var $tbody = d3.select("tbody");
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Set values in input type date and time 
+var inputFieldDate = d3.select("#datetime");
+
